c64ef2b3d4fc831320448a3bbe6670b340683540
abdulkerimak <abdulkerim.ak@sahibinden.com>
2018-11-02 12:37:05 +0300

    te

--- kare.py
+++ kare.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-for i in range(0,11):
-    if i % 2 == 0:
-        print(pow(i, 2))
+#for i in range(0,11):
+#    if i % 2 == 0:
+#        print(pow(i, 2))
+
+
+while True:
+    sayi = raw_input("lütfen karesini almak istediğiniz sayıyı giriniz: " )
+    if sayi == "iptal":
+        break
+    else:
+        print(pow(int(sayi), 2))
