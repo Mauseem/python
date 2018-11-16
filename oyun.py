53b3fb8a57ace72e6871f71a76be2f540dc26f76
abdulkerimak <abdulkerim.ak@sahibinden.com>
2018-11-16 10:27:19 +0300

    oyun class

---
+++ oyun.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+class Oyun:
+	def __init__(self)
+	enerji = 50
+	para = 100
+	fabrika = 4
+	isci = 10
+	print "enerji:", enerji
+	print "para:", para
+	print "fabrika:", fabrika
+	print "işçi:", isci
+
+macera = Oyun()
