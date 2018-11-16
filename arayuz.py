06f371d2f02111fd4bd6858423331eb22a673d95
abdulkerimak <abdulkerim.ak@sahibinden.com>
2018-11-16 10:44:00 +0300

    update

---
+++ arayuz.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from Tkinter import *
+
+class Arayuz:
+	def __init__(self):
+		pencere = Tk()
+		dugme = Button(text="tamam")
+		dugme.pack()
+		pencere.mainloop()
+uygulama = Arayuz()
--- oyun.py
+++ oyun.py
@@ -2,14 +2,14 @@
 # -*- coding: utf-8 -*-
 
 class Oyun:
-	def __init__(self)
-	enerji = 50
-	para = 100
-	fabrika = 4
-	isci = 10
-	print "enerji:", enerji
-	print "para:", para
-	print "fabrika:", fabrika
-	print "işçi:", isci
+	def __init__(self):
+		enerji = 50
+		para = 100
+		fabrika = 4
+		isci = 10
+		print "enerji:", enerji
+		print "para:", para
+		print "fabrika:", fabrika
+		print "işçi:", isci
 
 macera = Oyun()
