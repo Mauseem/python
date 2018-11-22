8262b255e599cae7e9aba53a7da9ba79cca3fe15
abdulkerimak <abdulkerim.ak@sahibinden.com>
2018-11-22 11:27:59 +0300

    kerem python dersleri

--- oyun.py
+++ oyun.py
@@ -14,6 +14,36 @@ class Oyun:
         print  "fabrika:", self.fabrika
         print "işçi:",  self.isci
 
+    def fabrikakur(self, miktar):
+        if self.enerji > 3 and self.para > 10:
+            self.fabrika  =miktar + self.fabrika
+            self.enerji = self.enerji - 3
+            self.para = self.para - 10
+            print miktar, "adet fabrika kurdunuz Tebrikler"
+        else:
+            print   "yeni fabrika kuramazsınız. \ " \
+                    "yeterli kaynağınız yok !"
+
+
+class Dusman(Oyun):
+    def __init__(self):
+        Oyun.__init__(self)
+        self.ego = 0
+    def goster(self):
+        Oyun.goster(self)
+        print "ego:", self.ego
+
+
+    def fabrikayik(self, miktar):
+        macera.fabrika = macera.fabrika - miktar
+        self.ego = self.ego + 2
+        print "tebrikler. Oyuncunun ", miktar, \
+        "adet fabrikasını yıktınız"
+        print "üstelik egonuzda büyüdü"
+
+
+dusman = Dusman()
 macera = Oyun()
-macera.goster()
+dusman.fabrikayik(2)
+dusman.goster()
 
