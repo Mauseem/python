128b0d2ecc3a04e849beb47c8720f04449b4af33
kerem <6599791@gmail.com>
2020-03-02 23:20:52 +0300

    add

--- dev.py
+++ dev.py
@@ -137,21 +137,21 @@
 
 #inheritance lesson
 
-class Mammal:
-    def walk(self):
-        print("walk")
-
-class Dog(Mammal):
-    def bark(self):
-        print("bark")
-
-
-class Cat(Mammal):
-    pass
-
-dog1 = Dog()
-dog1.walk()
-dog1.bark()
+# class Mammal:
+#     def walk(self):
+#         print("walk")
+#
+# class Dog(Mammal):
+#     def bark(self):
+#         print("bark")
+#
+#
+# class Cat(Mammal):
+#     pass
+#
+# dog1 = Dog()
+# dog1.walk()
+# dog1.bark()
 
 
 
