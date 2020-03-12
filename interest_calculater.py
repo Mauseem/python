318d27c5b686a636a84ef68b05de92aca6c3def0
kolpator <abdulkerimak@gmail.com>
2020-03-12 17:36:30 +0300

    's'

---
+++ interest_calculater.py
@@ -0,0 +1,40 @@
+def birikim_hesapla():
+    bkumulatif = 0
+    aylik_gelir = int(input('aylik toplm gelirinizi girini ormegin 120000   > '))
+    anapara = aylik_gelir * 12
+    zamorani = float(input('lutfen zam oraninini giriniz ornegin 0.18  >   '))
+    for x in range(1, 11, 1):
+        yillikzam = anapara * zamorani
+        #zamlimaas = anapara + yillikzam
+        print(f' yillik toplam gelir {anapara} lira')
+        anapara += yillikzam
+        bkumulatif += anapara
+    print(f' 10 yillik toplam   gelir {bkumulatif}  lira')
+
+
+
+def faiz_hesapla():
+    fkumulatif = 0
+    aylik_kira = int(input('lutfen kira tutarini giriniz  ornegin 1350  >   '))
+    zaman = int(input('lutfen zaman araligi giriniz  ornegin 6 yil icin 6 yada 1  yil icin 1 girebilirsiniz >  '))
+    zamorani = float(input('lutfen zam oraninini giriniz ornegin 0.18  >   '))
+    for x in range(1, zaman+1):
+        yillikzam = aylik_kira * zamorani
+        # zamlimaas = anapara + yillikzam
+        print(f'1 yillik kira toplami =  { round(aylik_kira * 12)} lira')
+        fkumulatif += aylik_kira * 12
+        aylik_kira += yillikzam
+        print(f'bu yil yapilan zam  {round(yillikzam)} lira')
+        print(f'yeni kira {round(aylik_kira)} lira  {x+1}.yil')
+    print(f'Toplam Odenen Kira = {zaman}  yil icin {fkumulatif} lira')
+
+
+birikim_hesapla()
+faiz_hesapla()
+
+# def masraf():
+#     birikim_hesapla()
+#     faiz_hesapla()
+#
+
+
