8cfeb57124111ab60e571cbc68429a99d016b549
kerem <6599791@gmail.com>
2021-07-23 13:03:42 +0300

    dd

--- def.py
+++ def.py
@@ -2,12 +2,12 @@
 # -*- coding: utf-8 -*-
 
 def dilekce_gonder(isim, tarih, gonderen):
-    print """\
-     Sayın %s,
+    print("""\
+     Sayın %s
      %s tarihinde yaptığımız başvurunun sonuçlandırılması
      hususunda yardımlarınızı rica ederiz.
      Saygılarımızla,
-     %s """  %(isim,tarih,gonderen)
+     %s """) % (isim, tarih, gonderen)
 
 
-dilekce_gonder(raw_input("alıcı adını giriniz"),raw_input("tarih griniz"),raw_input("gönderen adı giriniz"))
+dilekce_gonder(input("alıcı adını giriniz"),input("tarih griniz"),input("gönderen adı giriniz"))
